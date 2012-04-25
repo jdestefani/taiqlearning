aed6fcdf2d0b60a8ebdc7d16bc5e31a6c00da0b9
dondeste <dondeste@fb7a71bd-336b-81be-7278-45a5f5f1ece8>
2012-04-25 20:45:58 +0000

    First committ of the project.

---
+++ src/data/CellType.java
@@ -0,0 +1,11 @@
+package data;
+
+public enum CellType {
+	PLAIN,
+	BONUS,
+	WALL,
+	MALUS,
+	STARTPOINT,
+	ENDPOINT,
+	AGENT
+}
---
+++ src/data/QGridCell.java
@@ -0,0 +1,45 @@
+package data;
+
+import java.util.ArrayList;
+
+public class QGridCell {
+
+	private CellType cellType;
+	private ArrayList<QGridCell> allowedDestinations;
+	private double cellReward;
+	
+	public QGridCell() {
+		super();
+		this.cellType = CellType.PLAIN;
+		allowedDestinations = new ArrayList<QGridCell>();
+		cellReward = 0.0;
+	}
+	public QGridCell(CellType cellType) {
+		super();
+		this.cellType = cellType;
+		allowedDestinations = new ArrayList<QGridCell>();
+		cellReward = 0.0;
+	}
+	
+	public QGridCell(CellType cellType, double cellReward) {
+		super();
+		this.cellType = cellType;
+		allowedDestinations = new ArrayList<QGridCell>();
+		this.cellReward = cellReward;
+	}
+	public CellType getCellType() {
+		return cellType;
+	}
+	public void setCellType(CellType cellType) {
+		this.cellType = cellType;
+	}
+	public ArrayList<QGridCell> getAllowedDestinations() {
+		return allowedDestinations;
+	}
+	public void setAllowedDestinations(ArrayList<QGridCell> allowedDestinations) {
+		this.allowedDestinations = allowedDestinations;
+	}
+	
+	
+	
+}
