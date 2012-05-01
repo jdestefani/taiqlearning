a91a1f3cbd39e14ba093cac339d63876ca44b8a3
dondeste <dondeste@fb7a71bd-336b-81be-7278-45a5f5f1ece8>
2012-05-01 14:08:06 +0000

    Random map generation

--- trunk/src/data/CellType.java
+++ trunk/src/data/CellType.java
@@ -12,4 +12,10 @@ public enum CellType {
 	PORTAL2,
 	PORTAL3,
 	PORTAL4;
+	
+	public static double PLAINREWARD = 0.0;
+	public static double BONUSREWARD = 10.0;
+	public static double MALUSREWARD = -10.0;
+	public static double WALLREWARD = -1000.0;
+	public static double ENDPOINTREWARD = 100.0;
 }
